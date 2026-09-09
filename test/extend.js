e5d15a49f239a8c7d6cb9806eb95c625d94dd6bb
Seal <info@sealsecurity.io>
2026-09-09 06:01:19 +0000

    CVE-2026-84368

--- test/extend.js
+++ test/extend.js
@@ -566,6 +566,42 @@ describe('extension', () => {
         expect(special.validate(1).error).to.be.an.error('shit happens');
     });
 
+    it('does not pollute the prototype with a language named __proto__', () => {
+
+        try {
+            Joi.extend({
+                type: 'special',
+                base: Joi.string(),
+                messages: {
+                    ['__proto__']: { isAdmin: 'true' }
+                }
+            });
+
+            expect({}.isAdmin).to.not.exist();
+        }
+        finally {
+            delete Object.prototype.isAdmin;
+        }
+    });
+
+    it('does not pollute Object with a language named constructor', () => {
+
+        try {
+            Joi.extend({
+                type: 'special',
+                base: Joi.string(),
+                messages: {
+                    constructor: { 'string.base': 'shit happens' }
+                }
+            });
+
+            expect(Object['string.base']).to.not.exist();
+        }
+        finally {
+            delete Object['string.base'];
+        }
+    });
+
     it('overrides specific error messages with template', () => {
 
         const custom = Joi.extend({
